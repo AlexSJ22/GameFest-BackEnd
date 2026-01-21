5cd8e1ea5267447e1780d9917771ed5466eddf8c
AlexSJ22 <alexisjosue.guanosa@elorrieta-errekamari.com>
2026-01-21 01:34:24 +0100

    Added methods for events, games and user register

--- functions.php
+++ functions.php
@@ -4,33 +4,114 @@ define("USUARIO", "root");
 define("CLAVE", "");
 define("BBDD", "gamefest");
 
-function obtenerJuegos()
+function conectarBD()
 {
     $mysqli = new mysqli(SERVIDOR, USUARIO, CLAVE, BBDD);
     $mysqli->set_charset('utf8');
-    $sql = "SELECT * FROM games";
-    $resultado = $mysqli->query($sql);
-
-    $eventos = [];
-    while ($fila = $resultado->fetch_assoc()) {
-        $eventos[] = $fila;
+    if ($mysqli->connect_errno) {
+        print ("Error de conexion " . $mysqli->connect_error);
     }
-    echo json_encode($eventos);
-    $resultado->free();
-    $mysqli->close();
+    return $mysqli;
 }
-function obtenerEventos()
+
+/**
+ * Metodo para obtener juego mediante el id o sin el id
+ * @param mixed $id si obtiene el id lo usa si no usa la query por defecto  
+ * Retorna el array con todos los juegos o un solo juego
+ */
+function obtenerJuegos($id = null)
 {
-    $mysqli = new mysqli(SERVIDOR, USUARIO, CLAVE, BBDD);
-    $mysqli->set_charset('utf8');
-    $sql = "SELECT * FROM eventos";
-    $resultado = $mysqli->query($sql);
-    $eventos = [];
-    while ($fila = $resultado->fetch_assoc()) {
-        $eventos[] = $fila;
+    $mysqli = conectarBD();
+    $juegos = [];
+
+    try {
+        if ($id) {
+            $stmt = $mysqli->prepare("SELECT * FROM games WHERE id = ?;");
+            $stmt->bind_param('i', $id);
+        } else {
+            $stmt = $mysqli->prepare("SELECT * from games");
+        }
+        $stmt->execute();
+        $resultado = $stmt->get_result();
+        $juegos = $resultado->fetch_all(MYSQLI_ASSOC);
+
+        foreach ($juegos as $juego) {
+            if (isset($juego['plataformas'])) {
+                $juego['plataformas'] = json_decode($juego['plataformas']);
+            }
+        }
+
+        if ($id && count($juegos) > 0) {
+            return $juegos[0];
+        }
+
+        return $juegos;
+
+    } catch (Exception $e) {
+        return ["error en obtener juegos" => $e->getMessage()];
+    } finally {
+        $mysqli->close();
     }
-    echo json_encode($eventos);
-    $resultado->free();
-    $mysqli->close();
+}
+/**
+ * Metodo para obtener todos los eventos mediante el id o sin el id
+ * @param mixed $id 
+ * Retorna array con todos los eventos o un evento
+ */
+function obtenerEventos($id = null)
+{
+    $mysqli = conectarBD();
+    $eventos = [];
+
+    try {
+        if ($id) {
+            $stmt = $mysqli->prepare("SELECT * FROM events WHERE id = ?;");
+            $stmt->bind_param('i', $id);
+        } else {
+            $stmt = $mysqli->prepare("SELECT * from events ORDER BY fecha ASC");
+        }
+        $stmt->execute();
+        $resultado = $stmt->get_result();
+        $eventos = $resultado->fetch_all(MYSQLI_ASSOC);
+
+        if ($id && count($eventos) > 0) {
+            return $eventos[0];
+        }
+
+        return $eventos;
+
+    } catch (Exception $e) {
+        return ["error obtener eventos" => $e->getMessage()];
+    } finally {
+        $mysqli->close();
+    }
+}
+/**
+ *etodo para registrar un usuario en la base de datos
+ * @param mixed $username se proporciona el usuario 
+ * @param mixed $email el email
+ * @param mixed $password y la contrasena
+ * @return string Devuelve un texto de confirmacion del registro del usuario
+ */
+function registrarUsuario($username, $email, $password)
+{
+    $mysqli = conectarBD();
+    try {
+        $hash = password_hash($password, PASSWORD_BCRYPT);
+        $stmt = $mysqli->prepare("INSERT INTO users (username,email,password_hash,role) VALUES (?,?,?,'user');");
+        $stmt->bind_param("sss", $username, $email, $hash);
+
+        if ($stmt->execute()) {
+            return "Usuario registrado correctamente, filas devueltas " + $stmt->affected_rows;
+        } else {
+            return "No se pudo registrar el usuario";
+        }
+
+
+    } catch (Exception $e) {
+        return "Error en registrar usuario: " . $e->getMessage();
+    } finally {
+        $mysqli->close();
+    }
 }
 ?>
--- pruebas.php
+++ pruebas.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL);
 require_once "functions.php";
-header("Content-Type: application/json");
-obtenerJuegos();
+header("Content-Type: application/json; charset=utf-8'");
+//echo json_encode(obtenerEventos(), JSON_UNESCAPED_UNICODE);
 ?>

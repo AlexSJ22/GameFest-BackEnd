68ef2e7d2734baa13f98ddc7971c26e705d5e90d
AlexSJ22 <alexisjosue.guanosa@elorrieta-errekamari.com>
2026-01-24 21:42:36 +0100

    Prepare branch for merge with feat/APIcreation, removing unnecessary files

--- logOut.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-require_once 'functions.php';
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    enviarJSON(['success' => false, 'message' => 'Método no permitido'], 405);
-}
-$resultado = logout();
-enviarJSON($resultado, 200);
-?>
